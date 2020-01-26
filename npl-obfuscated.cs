e661c5a814f31cc7d64971cd5df6484954e27f55
reigningshells <development@reigningshells.com>
2020-01-25 22:31:47 -0500

    Update npl-obfuscated.cs
    Needed something quick and dirty for an assessment, wanted some more shell functionality so fixed output and added some tab completion.  Not pretty but served it's purpose.

--- npl-obfuscated.cs
+++ npl-obfuscated.cs
@@ -1,7 +1,12 @@
 using System;
+using System.IO;
 using System.Collections.ObjectModel;
 using System.Management.Automation;
 using System.Reflection;
+using System.Text;
+using System.Linq;
+using System.Globalization;
+using System.Collections.Generic;
 
 /*
 Author: Reigning Shells
@@ -21,7 +26,7 @@ without it, will trigger an alert at compile time or when npl.exe is dropped to 
 
 How to compile:
 ===============
-c:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /reference:C:\Windows\Microsoft.NET\assembly\GAC_MSIL\System.Management.Automation\v4.0_3.0.0.0__31bf3856ad364e35\system.management.automation.dll /out:npl.exe npl.cs
+c:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /reference:C:\Windows\Microsoft.NET\assembly\GAC_MSIL\System.Management.Automation\v4.0_3.0.0.0__31bf3856ad364e35\system.management.automation.dll /out:npl.exe npl-obfuscated.cs
 
 How to use:
 ============
@@ -39,6 +44,7 @@ namespace npl
     {
         static void Main(string[] args)
         {
+
             PowerShell ps = PowerShell.Create();
 
             // Disable Logging
@@ -65,12 +71,12 @@ namespace npl
             {
                 if (args[0].ToLower() == "-encode")
                 {
-                    if(args.Length == 2)
+                    if (args.Length == 2)
                     {
                         Byte[] bytes = System.Text.Encoding.Unicode.GetBytes(args[1]);
                         Console.WriteLine(System.Convert.ToBase64String(bytes));
                     }
-                    else 
+                    else
                     {
                         Console.WriteLine("usage: npl.exe -encode \"& commands; separated; by; semicolons;\"");
                     }
@@ -95,8 +101,7 @@ namespace npl
                         string pwd = ps.Invoke()[0].ToString();
                         string prompt = "PS " + pwd + "> ";
 
-                        Console.Write(prompt);
-                        var input = Console.ReadLine();
+                        string input = TabableReadLine(ps, prompt, pwd);
 
                         if (string.IsNullOrEmpty(input))
                         {
@@ -108,12 +113,11 @@ namespace npl
                             break;
                         }
 
-                        ps.AddScript(input);
-                        Invoke(ps);
+                        Invoke(ps, input);
                         ps.Commands.Clear();
                     }
                 }
-                else 
+                else
                 {
                     if (args[0].ToLower() == "-encodedcommand" || args[0].ToLower() == "-enc")
                     {
@@ -122,8 +126,7 @@ namespace npl
                         {
                             script += System.Text.Encoding.Unicode.GetString(System.Convert.FromBase64String(args[argidx]));
                         }
-                        ps.AddScript(script);
-                        Invoke(ps);
+                        Invoke(ps, script);
                     }
                     else
                     {
@@ -132,19 +135,115 @@ namespace npl
                         {
                             script += @args[argidx];
                         }
-                        ps.AddScript(script);
-                        Invoke(ps);
+                        Invoke(ps, script);
                     }
 
-                }                
+                }
             }
             else
             {
                 Console.WriteLine("\r\nusage:\r\nnpl.exe -shell\r\nnpl.exe \"{powershell single command}\"\r\nnpl.exe \"& {commands; semi-colon; separated}\"\r\nnpl.exe -encodedcommand {base64_encoded_command}\r\nnpl.exe -encode \"commands to encode to base64\"\r\nnpl.exe -decode {base64_encoded_command}");
             }
         }
-        private static void Invoke(PowerShell ps)
+ 
+        private static string TabableReadLine(PowerShell ps, string prompt, string pwd)
         {
+            Console.Write(prompt);
+            var builder = new StringBuilder();
+            var input = Console.ReadKey(intercept: true);
+
+            while (input.Key != ConsoleKey.Enter)
+            {
+                var currentInput = builder.ToString();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.Tab:
+                      
+                        var currentParams = currentInput.Split(' ');
+                        string currentParam = currentParams[currentParams.Length - 1];
+
+                        // Get matching PowerShell cmdlets
+                        ps.AddScript("Get-Command -name \"" + currentParam + "*\" | Select name | Out-String");
+                        PSObject output = ps.Invoke()[0];
+                        string[] names = Array.FindAll(output.ToString().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None), x => x.StartsWith(currentParam));
+
+                        // Get matching files in current directory
+                        var data = Directory.EnumerateFileSystemEntries(pwd, "*").Select(Path.GetFileName);
+                        var matches = data.Where(item => item != currentParam && item.StartsWith(currentParam, true, CultureInfo.InvariantCulture));
+
+                        matches = names.Concat(matches);
+                        if (matches != null && matches.Any())
+                        {
+                            if (matches.Count() == 1)
+                            {
+                                currentParams[currentParams.Length - 1] = matches.First();
+                            }
+                            else
+                            {
+                                Console.WriteLine("");
+                                foreach (string match in matches)
+                                {
+                                    Console.WriteLine(match);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            input = Console.ReadKey(intercept: true);
+                            continue;
+                        }
+
+                        ClearCurrentLine(prompt);
+                        builder.Clear();
+
+                        string currentCmd = string.Join(" ", currentParams, 0, currentParams.Length);
+
+                        Console.Write(currentCmd);
+                        builder.Append(currentCmd);
+                        break;
+                    case ConsoleKey.Backspace:
+                        if (currentInput.Length > 0)
+                        {
+                            builder.Remove(builder.Length - 1, 1);
+                            ClearCurrentLine(prompt);
+
+                            currentInput = currentInput.Any() ? currentInput.Remove(currentInput.Length - 1, 1) : string.Empty;
+                            Console.Write(currentInput);
+                        }
+                        else
+                        {
+                            ClearCurrentLine(prompt);
+                        }
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        builder.Remove(builder.Length - 1, 1);
+                        Console.SetCursorPosition(prompt.Length + currentInput.Length - 1, Console.CursorTop);
+                        break;
+                    default:
+                        var key = input.KeyChar;
+                        builder.Append(key);
+                        Console.Write(key);
+                        break;
+                }
+
+                input = Console.ReadKey(intercept: true);
+            }
+            Console.WriteLine(input.KeyChar);
+            return builder.ToString();
+        }
+
+        private static void ClearCurrentLine(string currentPrompt)
+        {
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(currentPrompt.PadRight(Console.WindowWidth - currentPrompt.Length, ' '));
+            Console.SetCursorPosition(currentPrompt.Length, Console.CursorTop);
+        }
+
+        private static void Invoke(PowerShell ps, string input)
+        {
+            string script = input + " 2>&1 | Out-String";
+            ps.AddScript(script);
             Collection<PSObject> output = null;
 
             try
@@ -155,19 +254,12 @@ namespace npl
             {
                 Console.WriteLine("Error while executing the script.\r\n" + e.Message.ToString());
             }
-
-            if (output != null)
+            if (output != null && output.Count > 0)
             {
-                Console.WriteLine("");
-
-                foreach (PSObject rtnItem in output)
+                string[] lines = output[0].ToString().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+                foreach (string line in lines)
                 {
-                    Console.WriteLine(rtnItem.ToString());
-                }
-
-                if (output.Count > 0)
-                {
-                    Console.WriteLine("");
+                        Console.WriteLine(line.TrimEnd());
                 }
             }
         }
